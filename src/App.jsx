41e16d4c5c2de9ccad6f6ef76a4ae95f3aa1a237
ogel <yocorangelo18@gmail.com>
2024-06-12 02:20:30 +0800

    add: require sa text field

--- src/App.jsx
+++ src/App.jsx
@@ -156,6 +156,7 @@ function App() {
                           onChange={(e) => setDescription(e.target.value)}
                           id="description"
                           rows="4"
+                           required
                           className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                           placeholder="Write product description here"
                         ></textarea>
